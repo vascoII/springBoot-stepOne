935bd78a2af03b21b367f58d0346beb40117746a
Fabrice Da Silva <f-dasilva2@autodistribution.com>
2020-04-23 23:18:28 +0200

    added: Customer entity, Customer mongo repo

--- src/main/java/rubusdata/location/LoadDatabase.java
+++ src/main/java/rubusdata/location/LoadDatabase.java
@@ -1,5 +1,5 @@
 package rubusdata.location;
-
+/*
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.CommandLineRunner;
@@ -20,4 +20,53 @@ class LoadDatabase {
             log.info("Preloading " + orderRepository.save(new Order("iPhone", Status.IN_PROGRESS)));
         };
     }
+}*/
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import rubusdata.location.domain.Customer;
+import rubusdata.location.domain.CustomerRepository;
+
+@SpringBootApplication
+public class LoadDatabase implements CommandLineRunner {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadDatabase.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        repository.deleteAll();
+
+        // save a couple of customers
+        repository.save(new Customer("Alice", "Smith"));
+        repository.save(new Customer("Bob", "Smith"));
+
+        // fetch all customers
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Customer customer : repository.findAll()) {
+            System.out.println(customer);
+        }
+        System.out.println();
+
+        // fetch an individual customer
+        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("--------------------------------");
+        System.out.println(repository.findByFirstName("Alice"));
+
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (Customer customer : repository.findByLastName("Smith")) {
+            System.out.println(customer);
+        }
+
+    }
+
 }
